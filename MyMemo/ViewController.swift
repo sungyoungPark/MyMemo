b3a7bdc072f5920fc89e91246c70d0495b08ad4c
sungyoungPark <sygitter1491012@gmail.com>
2020-02-11 00:21:21 +0900

    Initial Commit

---
+++ MyMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMemo
+//
+//  Created by 박성영 on 11/02/2020.
+//  Copyright © 2020 박성영. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
